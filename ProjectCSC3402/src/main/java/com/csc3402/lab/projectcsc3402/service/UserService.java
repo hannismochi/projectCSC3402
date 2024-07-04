8fb39629bb681ff7778bf3b08cf6a2a47a994771
hannismochi <115637245+hannismochi@users.noreply.github.com>
2024-07-04 21:11:02 +0800

    Delete ProjectCSC3402 directory

--- ProjectCSC3402/src/main/java/com/csc3402/lab/projectcsc3402/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.csc3402.lab.projectcsc3402.service;
-
-import com.csc3402.lab.projectcsc3402.dto.UserDto;
-import com.csc3402.lab.projectcsc3402.model.User;
-
-public interface UserService {
-    void saveUser(UserDto userDto);
-    User findUserByEmail(String email);
-}
